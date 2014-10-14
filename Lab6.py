ff3706c42fa6437cf171d61960dc295b2d69a01a
nickihilker <nickihilker@gmail.com>
2014-10-14 13:28:36 -0500

    First Commit

---
+++ Lab6.py
@@ -0,0 +1,34 @@
+#25
+total = 0
+print "How many numbers do you want to add together?"
+userInput1 = int(raw_input())
+
+for x in range(userInput1):
+    print "Enter a number"
+    userInput1 = int(raw_input())
+    total = total + userInput1
+print total
+
+#50
+
+totalCost = []
+print "How many numbers do you want to add together?"
+userInput2 = int(raw_input())
+
+for x in range(userInput2):
+    print "Enter a number"
+    userInput2 = int(raw_input())
+    totalCost.append(userInput2)
+print sum(totalCost)
+
+#75
+#factorial = 
+total = []
+print "What number do you want to take the factorial of?"
+userInput3 = int(raw_input())
+for x in range(userInput3):
+    total = total * x
+print total
+
+#100 dlkfjhdkfj
+
